802b3457198184d31dc835015c03a519ee80db64
rhg4656 <llloe@naver.com>
2024-05-21 21:49:58 +0900

    5월 21일 todoList 리팩토링 시작

---
+++ workspace/5_vue-app2/1_prop-test/src/App4.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <h1>유저 정보 조회</h1>
+  <ul>
+    <CheckboxItem v-for='user in users' :key='user.number' :user='user'/>
+  </ul>
+</template>
+<script>
+import CheckboxItem from './components/CheckboxItem4.vue';
+import User from './user.js';
+
+  export default {
+    name : 'App4',
+    components : {CheckboxItem},
+
+    data() {
+      return {
+        users : [
+          new User(1, '류호근', true),
+          new User(2, '홍길동', true),
+          new User(3, '신짱구', true),
+          { number : 4, name:'임꺽정', checked : true}
+        ]
+      }
+    },
+  }
+</script>
+<style lang="">
+  
+</style>
